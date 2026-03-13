24b9f4ad4c92e8555573e9687d50aa8b956ddc47
jackmford <jackmitchellfordyce@gmail.com>
2026-03-12 19:27:11 -0500

    reorg settings

--- init.lua
+++ init.lua
@@ -84,6 +84,10 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+-- load core settings
+require 'custom.core.diagnostics'
+require 'custom.core.options'
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)

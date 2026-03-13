cd1c004e67b560a57085e9761e3597955bc5760e
jackmford <jackmitchellfordyce@gmail.com>
2026-03-12 19:08:50 -0500

    working on nvim config

--- lua/custom/plugins/keymaps.lua
+++ lua/custom/plugins/keymaps.lua
@@ -20,4 +20,12 @@ return {
 
   -- Markdown Prev
   vim.keymap.set('n', '<leader>cp', '<cmd>MarkdownPreviewToggle<CR>'),
+
+  -- Clear highlights on search when pressing <Esc> in normal mode
+  --  See `:help hlsearch`
+  vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>'),
+
+  -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+  -- or just use <C-\><C-n> to exit terminal mode
+  vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }),
 }
